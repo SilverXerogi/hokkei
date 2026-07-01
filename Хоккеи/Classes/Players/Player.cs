81b8db7b7463f4f7ea0341b905f9edbd15e15126
Максим <@NUCBOX_M5ULTRA>
2026-07-01 17:25:45 +0300

    Первые классы

---
+++ Хоккеи/Classes/Players/Player.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Хоккеи.Classes.enums;
+using Хоккеи.Classes.equipment;
+
+namespace Хоккеи
+{
+    abstract public class Player
+    {
+        public int id { get; private set; }
+        public string Name { get; private set; }
+        public PlayerRole Role { get; private set; }
+        public int Agility { get; private set; }
+        public int Strength { get; private set; }
+        public int Stamina { get; private set; }
+
+        public float maxEnergy { get; private set; }
+        public float Energy { get; set; }
+        public Equipment Gear { get; set; }
+        public bool IsOnIce { get; set; }
+
+        public bool IsGoalie { get { return (Role == PlayerRole.Goalie); } }
+
+        protected Player(int id, string name, PlayerRole role, int agility, int strength, int stamina, float maxEnergy, float energy, Equipment gear, bool isOnIce)
+        {
+            this.id = id;
+            Name = name;
+            Role = role;
+            Agility = agility;
+            Strength = strength;
+            Stamina = stamina;
+            this.maxEnergy = maxEnergy;
+            Energy = energy;
+        }
+    }
+ }
---
+++ Хоккеи/Classes/equipment/Equipment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Хоккеи.Classes.equipment
+{
+    public class Equipment
+    {
+        public string Name {  get; private set; }
+        public float AttackBonus { get; private set; }
+        public float DefenceBonus { get; private set; }
+        public Equipment (string name, float attackBonus, float defenceBonus)
+        {
+            Name = name;
+            AttackBonus = attackBonus;
+            DefenceBonus = defenceBonus;
+        }
+    }
+}
